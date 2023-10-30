0856c04b5936c62366ef737f7779e71c524f8d13
mhieeu <abcxyz123124@gmail.com>
2023-10-31 00:04:49 +0700

    add css for login page

--- demosecurity/src/main/java/com/example/demosecurity/controller/DemoController.java
+++ demosecurity/src/main/java/com/example/demosecurity/controller/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoController {
     @GetMapping("/")
     public String showHome(){
-        return "home";
+        return "login";
     }
 
     @GetMapping("/leaders")
--- demosecurity/src/main/java/com/example/demosecurity/security/DemoSecurityConfig.java
+++ demosecurity/src/main/java/com/example/demosecurity/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
